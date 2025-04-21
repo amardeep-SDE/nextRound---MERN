c5bb517566406f2648d2dc8b5c798e963e3c87d3
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-04-21 14:55:30 +0530

    fix: chat and auth section

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import useChat from "../hooks/useChat";
 // import { setMessages } from "../store/chatSlice";
 import useOtherUsers from "../hooks/useOtherUsers";
