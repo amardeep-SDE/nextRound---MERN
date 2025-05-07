890263a60ff00947b810e00299103fdff13471b3
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-05-07 22:27:07 +0530

    fix:validation

--- client/src/schema/validationSchemas.jsx
+++ client/src/schema/validationSchemas.jsx
@@ -4,13 +4,13 @@ import { z } from "zod";
 export const signupSchema = z.object({
   username: z.string().min(4, "Name must be at least 4 characters"),
   email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
   phone: z.string().min(10, "Phone number must be at least 10 digits"),
 });
 
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
 // Zod validation schema
