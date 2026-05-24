f74e6f0ac2ddc82fa40fbeb9fce7a6779be311ae
Amardeep Dwivedi <amardeepdwivedi1494@gmail.com>
2026-05-24 22:24:19 +0530

    fix: validation

--- client/src/schema/validationSchemas.jsx
+++ client/src/schema/validationSchemas.jsx
@@ -30,8 +30,8 @@ export const forgotPasswordSchema = z.object({
 
 export const resetPasswordSchema = z
   .object({
-    password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z.string().min(6, "Confirm password is required"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    confirmPassword: z.string().min(8, "Confirm password is required"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
